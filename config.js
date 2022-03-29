49ecac3966d53a4400f2e040beaa1c867cd0bdda
ByteAKA8Bit <zj_hasnoemail@163.com>
2022-03-29 12:31:10 +0800

    create

---
+++ config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlPlugin = require('./lib/plugins/html-plugin');
+
+module.exports = {
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          path.resolve(__dirname, 'lib/loaders/remove-console-loader.js'),
+          path.resolve(__dirname, 'lib/loaders/add-author-loader.js'),
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+};
